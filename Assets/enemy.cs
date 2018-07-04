44a7470050ceebed5bce6567aef6ffca4e3a1198
claudiom4sir <claudio_rota@hotmail.it>
2018-07-04 17:36:54 +0200

    import from claudio PC

---
+++ Assets/enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		target = wayPoints.points[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+            GetNextWaypoint();
+	}
+
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= wayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = wayPoints.points[waypointIndex];
+    }
+
+}
